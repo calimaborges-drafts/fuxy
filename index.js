81182d4ae82e569a5ad696555d5259c7f76eb49c
Carlos Augusto Borges <calimaborges@gmail.com>
2015-11-23 21:48:20 -0200

    Now parses URL

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var net = require("net");
 var request = require('superagent');
+var url = require("url");
 
 var proxyUrl = process.env.http_proxy;
 
@@ -9,6 +10,37 @@ var debugfunc = function(name) {
     };
 };
 
+var urlFromData = function(data) {
+    var pattern = /(\S+)\s+(\S+)\s+(\S+)/i
+    var info = pattern.exec(data);
+    var method = info[1];
+    var uri = info[2];
+    var httpVersion = info[3];
+
+    return uri;
+};
+
+var hostFromUrl = function(uri) {
+    uri = url.parse(uri);
+
+    return uri.hostname;
+};
+
+var portFromUrl = function(uri) {
+    uri = url.parse(uri);
+
+    if(!uri.port) {
+        var protocol = uri.protocol.substring(0, uri.protocol.length - 1);
+        switch (protocol) {
+            case 'http': return 80;
+            case 'https': return 443;
+            default: return 80;
+        }
+    }
+
+    return uri.port;
+}
+
 module.exports = function(serverHost, serverPort, port) {
     var server = net.createServer( function(socket) {
         socket.on('connect', debugfunc('connect'));
@@ -16,10 +48,12 @@ module.exports = function(serverHost, serverPort, port) {
             console.log("Connection closed with proxy client");
         });
         socket.on('data', function(data) {
+            var url = urlFromData(data);
+
             var message = {
                 type: 'data',
-                host: 'test.carlosborg.es',
-                port: '80',
+                host: hostFromUrl(url),
+                port: portFromUrl(url),
                 content: data.toString('base64')
             }
 
