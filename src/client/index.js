fe7edaa07a1fe81ce32d6ac360bbf86c696a3085
Carlos Augusto Borges <calimaborges@gmail.com>
2017-03-29 15:31:34 -0300

    Removind debug strings

--- src/client/index.js
+++ src/client/index.js
@@ -1,83 +1,51 @@
 import net from 'net';
-import Debug from '../shared/Debug';
 import { portFromUrl, hostFromUrl } from '../shared/http-parsing';
 import { encode } from '../shared/base64';
 
-const debug = new Debug(process.env.NODE_ENV === 'development');
 const httpProxy = process.env.http_proxy;
 
 const createTunnel = (serverHost, serverPort, data, socket) => {
-    debug.d("[CLIENT] Creating tunnel for socket");
-
     const tunnel = new net.Socket();
-
     let connectHost = serverHost;
     let connectPort = serverPort;
 
-    debug.attachListeners(tunnel, '[CLIENT-TUNNEL]', ['connect', 'close', 'drain', 'end', 'lookup', 'timeout', 'data']);
-    tunnel.on('error', function(err) {
-        console.error("[CLIENT-TUNNEL] " + err.toString());
-    });
+    tunnel.on('error', (err) => console.error("[CLIENT-TUNNEL] " + err.toString()));
 
     if (httpProxy) {
         connectPort = portFromUrl(httpProxy);
         connectHost = hostFromUrl(httpProxy);
     }
 
-    tunnel.connect(connectPort, connectHost, function() {
-        debug.d("[CLIENT-TUNNEL] -> " + connectHost + ":" + connectPort + " -> " + serverHost + ":" + serverPort);
-        debug.d("[CLIENT-TUNNEL] ---- Start Data ---->");
-        debug.d(data.toString());
-        debug.d("[CLIENT-TUNNEL] ---- End Data ---->");
-
-        tunnel.write(data);
-    });
-
-    tunnel.on('end', function() {
-        socket.end();
-    });
-
-    tunnel.on('data', function(data) {
-        debug.d("[CLIENT-TUNNEL] <---- Start Data ----");
-        debug.d(data.toString());
-        debug.d("[CLIENT-TUNNEL] <---- End Data ----");
-        socket.write(data);
-    });
-
+    tunnel.connect(connectPort, connectHost, () => tunnel.write(data));
+    tunnel.on('end', () => socket.end());
+    tunnel.on('data', (data) => socket.write(data));
     return tunnel;
 };
 
 const createServer = (serverHost, serverPort, clientPort) => {
-    const server = net.createServer( function(socket) {
-        debug.attachListeners(socket, '[CLIENT]', ['connect', 'close', 'drain', 'end', 'error', 'lookup', 'timeout', 'data']);
 
-        socket.on('error', function(err) {
-            console.error("[CLIENT] " + err.toString());
-        });
+    const server = net.createServer( (socket) => {
+        socket.on('error', (err) => console.error("[CLIENT] " + err.toString()));
 
-        socket.on('data', function(data) {
+        socket.on('data', (data) => {
             data = encode(data);
-            data = "POST http://" + serverHost + ":" + serverPort + " HTTP/1.0\r\n" +
-                   "Host: " + serverHost + ":" + serverPort + "\r\n" +
-                   "Content-Type: application/json\r\n" +
-                   "{ \"data\": \"" + data + "\"}" +
-                   "\r\n\r\n";
-            if (!socket.tunnel) {
-                socket.tunnel = createTunnel(serverHost, serverPort, data, socket);
-            } else {
-                socket.tunnel.write(data);
-            }
 
-            debug.d("[CLIENT] <---- Start Data ----");
-            debug.d(data.toString());
-            debug.d("[CLIENT] <---- End Data ----");
+            data = `POST http://${serverHost}:${serverPort} HTTP/1.0\r
+                    Host: ${serverHost}:${serverPort}\r
+                    Content-Type: application/json\r
+                    { "data": "${data}"}
+                    \r
+                    \r
+                    `;
+
+            if (!socket.tunnel)
+                socket.tunnel = createTunnel(serverHost, serverPort, data, socket);
+            else
+                socket.tunnel.write(data);
         });
     });
 
     return server.listen(clientPort);
 };
 
-module.exports = (serverHost, serverPort, clientPort) => {
-    debug.d("[CLIENT] " + clientPort + " -> " + serverHost + ":" + serverPort);
-    return createServer(serverHost, serverPort, clientPort);
-};
+export default (serverHost, serverPort, clientPort) => createServer(serverHost, serverPort, clientPort);
--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,16 @@
 import net from 'net';
 import { infoFromString, hostFromUrl, portFromUrl } from '../shared/http-parsing';
-import Debug from '../shared/Debug';
 import { decode } from '../shared/base64';
 
-const debug = new Debug(process.env.NODE_ENV === 'development');
 const httpProxy = process.env.http_proxy;
 
 const createTunnel = (data, socket) => {
-    debug.d("[SERVER] Creating socket tunnel");
 
     const httpInfo = infoFromString(data);
     let uri = httpInfo.uri;
 
-    if (httpInfo.method === 'CONNECT') {
+    if (httpInfo.method === 'CONNECT')
         uri = "connect://" + uri;
-    }
 
     const host = hostFromUrl(uri);
     const port = portFromUrl(uri);
@@ -23,81 +19,49 @@ const createTunnel = (data, socket) => {
     let    connectHost = host;
     let    connectPort = port;
 
-    // Caso servidor necessite de proxy
     if (httpProxy) {
         connectHost = hostFromUrl(httpProxy);
         connectPort = portFromUrl(httpProxy);
     }
 
-    // Captura eventos não tratados para debug
-    debug.attachListeners(tunnel, '[SERVER-TUNNEL]', ['connect', 'close', 'drain', 'end', 'lookup', 'timeout', 'data']);
+    tunnel.on('error', (err) => console.error("[SERVER-TUNNEL] " + err.toString()));
 
-    // Caputra erros
-    tunnel.on('error', function(err) {
-        console.error("[SERVER-TUNNEL] " + err.toString());
-    });
+    tunnel.on('end', () => socket.end());
 
-    tunnel.on('end', function() {
-        socket.end();
-    });
+    // Connect to host and send first chunk
+    tunnel.connect(connectPort, connectHost, () => {
 
-    // Estabelece conexão com servidor destino e envia primeiro 'chunk'
-    tunnel.connect(connectPort, connectHost, function() {
         if (httpInfo.method === 'CONNECT') {
             data = "HTTP/1.0 200 Connection established\r\n\r\n";
             socket.write(data);
-        } else {
+        } else
             tunnel.write(data);
-        }
-
-        debug.d("[SERVER-TUNNEL] -> " + connectHost + ":" + connectPort + " -> " + host + ":" + port );
-        debug.d("[SERVER-TUNNEL] ---- Start Data ---->");
-        debug.d(data.toString());
-        debug.d("[SERVER-TUNNEL] ---- End Data ---->");
     });
 
-    // Quando receber um dado encaminha para o socket que se comunica com o
-    // cliente do proxy.
-    tunnel.on('data', function(data) {
-        debug.d("[SERVER-TUNNEL] <---- Start Data ----");
-        debug.d(data.toString());
-        debug.d("[SERVER-TUNNEL] <---- End Data ----");
-        socket.write(data);
-    });
+    // Forward data to proxy client
+    tunnel.on('data', (data) => socket.write(data));
 
-    // Retorna o tunnel criado
     return tunnel;
 };
 
 const createServer = (serverPort) => {
     let splitted = null;
     const server = net.createServer( (socket) => {
-        debug.attachListeners(socket, '[SERVER]', ['connect', 'close', 'drain', 'end', 'error', 'lookup', 'timeout', 'data']);
 
         socket.on('data', (data) => {
-            debug.d(data.toString());
-            debug.d(data.toString().split("\r\n"));
             splitted = data.toString().split("\r\n");
             data = splitted[3];
             data = JSON.parse(data).data;
-            debug.d(data);
             data = decode(new Buffer(data.toString(), 'base64'));
-            if (!socket.tunnel) {
-                socket.tunnel = createTunnel(data, socket);
-            } else {
-                socket.tunnel.write(data);
-            }
 
-            debug.d("[SERVER] <---- Start Data ----");
-            debug.d(data.toString());
-            debug.d("[SERVER] <---- End Data ----");
+            if (!socket.tunnel)
+                socket.tunnel = createTunnel(data, socket);
+            else
+                socket.tunnel.write(data);
         });
     });
 
     return server.listen(serverPort);
 };
 
-export default (serverPort) => {
-    debug.d(`[SERVER] -> ${serverPort}`);
-    return createServer(serverPort);
-};
+export default (serverPort) => createServer(serverPort);
