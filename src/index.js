8da32cedcae769e7b87aaa00ffa66a8d1791b0e8
Carlos Augusto Borges <calimaborges@gmail.com>
2017-03-29 14:55:28 -0300

    Adding editorconfig

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import client from './client';
-import server from './server';
+import client from "./client";
+import server from "./server";
 
-let type = 'server';
+let type = "server";
 
 if (process.argv.length > 2) {
-    if (process.argv[2].indexOf('client') > 0) {
-        type = 'client';
+    if (process.argv[2].indexOf("client") > 0) {
+        type = "client";
     }
 }
 
-if (type === 'client') {
-    // require("./client/index")("kali-xypro.herokuapp.com", 80, 8888);
+if (type === "client") {
     client("127.0.0.1", 8889, 8888);
 } else {
     server(process.env.PORT || 8889);
